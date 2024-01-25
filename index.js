c4b70fafe20b92264141df8147f622ce5aff6b73
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-25 14:21:24 +0100

    adds new features

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log("ciao");
 const doSomething = (number1, number2) => {
   return number1 - number2;
 };
+
+const checkEvenOrOdd = (number1) => {
+  if (number1 % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
